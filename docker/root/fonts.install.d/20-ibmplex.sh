e61a1c93e1163d8a423bbec092ba3fbd2a9fca0d
hg-jt <hg-jt@users.noreply.github.com>
2022-12-27 11:29:19 -0500

    Changed default font to IBM Plex Sans.

--- docker/root/fonts.install.d/20-ibmplex.sh
+++ docker/root/fonts.install.d/20-ibmplex.sh
@@ -6,6 +6,15 @@ VERSION=6.1.1
 
 curl -L -O https://github.com/IBM/plex/archive/refs/tags/v$VERSION.tar.gz
 tar xzf v$VERSION.tar.gz
+
+# IBM Plex Mono
 mv plex-$VERSION/IBM-Plex-Mono/fonts/complete/otf /usr/share/fonts/opentype/ibmplexmono
+cp plex-$VERSION/LICENSE.txt /usr/share/fonts/opentype/ibmplexmono
+
+# IBM Plex Sans
 mv plex-$VERSION/IBM-Plex-Sans/fonts/complete/otf /usr/share/fonts/opentype/ibmplexsans
+cp plex-$VERSION/LICENSE.txt /usr/share/fonts/opentype/ibmplexsans
+
+# IBM Plex Serif
 mv plex-$VERSION/IBM-Plex-Serif/fonts/complete/otf /usr/share/fonts/opentype/ibmplexserif
+cp plex-$VERSION/LICENSE.txt /usr/share/fonts/opentype/ibmplexserif
