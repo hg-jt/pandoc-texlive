58f1fe5eb00e39a446975d34ee9c121c88bc399b
hg-jt <hg-jt@users.noreply.github.com>
2023-06-12 22:05:56 -0400

    Upgraded to Pandoc 3.1.3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,12 @@ is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/), and this
 project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 
+## [Unreleased]
+
+### Changed
+
+- Upgraded Pandoc to 3.1.3.
+
 ## [20230226]
 
 ### Added
--- docker/docker-provision.sh
+++ docker/docker-provision.sh
@@ -41,7 +41,7 @@ curl -LSs \
 # ----------------------------------------
 # Install Pandoc
 # ----------------------------------------
-PANDOC_VERSION=2.19.2
+PANDOC_VERSION=3.1.3
 curl -L -O https://github.com/jgm/pandoc/releases/download/$PANDOC_VERSION/pandoc-$PANDOC_VERSION-1-amd64.deb
 apt-get install -y ./pandoc-$PANDOC_VERSION-1-amd64.deb
 rm pandoc-$PANDOC_VERSION-1-amd64.deb
